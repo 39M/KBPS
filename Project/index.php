f44202ad5d6249401d31c9508b1ada06de0afbee
Xivid <xividyzf@gmail.com>
2016-05-25 13:56:39 +0800

    landing screen

--- Project/index.php
+++ Project/index.php
@@ -1,11 +1,72 @@
-<h1>
-<?php
-/**
- * Created by PhpStorm.
- * User: Xivid
- * Date: 5/24/16
- * Time: 20:12
- */
-echo "It works!";
-?>
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Knowledge-Based Prescription System</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+   
+
+    <!-- Header -->
+    <header id="top" class="header">
+        <div class="text-vertical-center">
+            <h1>Knowledge-Based Prescription System</h1>
+            <h3>Select your role</h3>
+            <br>
+            <a href="doctor/" class="btn btn-dark btn-lg">I am a doctor</a>
+            <a href="admin/" class="btn btn-dark btn-lg">I am an administrator</a>
+        </div>
+    </header>
+
+    
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h4><strong>Knowledge-Based Prescription System</strong>
+                    </h4>
+                    <p>Yet Another Database Group<br>Xivid, Nil, Tony</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-phone fa-fw"></i> +86 13029990587</li>
+                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:kbps@vivid.name">kbps@vivid.name</a>
+                        </li>
+                    </ul>
+                    <br>
+                    <hr class="small">
+                    <p class="text-muted">Copyright &copy; Yet Another Database Group 2016</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
